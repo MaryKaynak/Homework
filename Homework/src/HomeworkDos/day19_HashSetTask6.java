37841a15f8e578ed7e214d5277d62f7205daac4e
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-05 17:37:47 -0500

    homework

---
+++ Homework/src/HomeworkDos/day19_HashSetTask6.java
@@ -0,0 +1,19 @@
+package HomeworkDos;
+
+import java.util.HashSet;
+
+public class day19_HashSetTask6 {
+    public static <HasSet> void main(String[] args) {
+        HashSet<String> fruit = new HashSet<>();
+        fruit.add("Apple");
+        fruit.add("Orange");
+        fruit.add("Plum");
+        fruit.add("Strawberry");
+        fruit.add("Melon");
+        System.out.println("Fruit:" + fruit);
+        HashSet<String> clonefruit = new HashSet<>();
+
+       clonefruit = (HashSet) fruit.clone();
+        System.out.println("Clone:" + clonefruit);
+    }
+}
