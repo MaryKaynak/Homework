949c598bc7480a40723c4d363b14f448ac86df6c
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-05 07:01:46 -0500

    homework

---
+++ Homework/src/HomeworkDos/day19_HashSet1.java
@@ -0,0 +1,16 @@
+package HomeworkDos;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class day19_HashSet1 {
+    public static void main(String[] args) {
+        Set<String> color = new HashSet<>();
+        color.add("Red");
+        color.add("Blue");
+        color.add("Black");
+        color.add("Pink");
+        color.add("Yellow");
+        System.out.println(color);
+    }
+}
