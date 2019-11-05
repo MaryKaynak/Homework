fb3b7bcbea9eba4612cb8ae01eabf964f4ef9bec
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-05 09:02:28 -0500

    homework

---
+++ Homework/src/HomeworkDos/day19_HashSetTask4.java
@@ -0,0 +1,21 @@
+package HomeworkDos;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class day19_HashSetTask4 {
+    public static void main(String[] args) {
+
+        Set<Integer> numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        System.out.println(numbers);
+         numbers.clear();
+        System.out.println(numbers);
+
+        }
+    }
