042842de4040163e9aa0689754ae36ef519ffcda
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-04 23:10:10 -0500

    homework

--- Homework/src/HomeworkDos.java
+++ Homework/src/HomeworkDos.java
@@ -1,5 +1,26 @@
 public class HomeworkDos {
     public static void main(String[] args) {
 
-    }
-}
+                int[] a1 = {5,8,7};
+                int[] a2 = {5,6,7};
+                boolean areEqual = true;
+                for(int i=0; i<a1.length;i++) {
+                    if (a1[i] == a2[i]) {
+                        areEqual = true;
+                    }else{
+                        areEqual=false;
+                        break;
+                    }
+                }
+                if(areEqual) {
+                    System.out.println("They are equal");
+                }else{
+                    System.out.println("They are not equal");
+                }
+
+
+            }
+        }
+
+
+
