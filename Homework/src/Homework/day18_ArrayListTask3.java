20371c32e5323be1f7f0025161629fb25452cdf5
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-08 15:48:53 -0500

    homework

---
+++ Homework/src/Homework/day18_ArrayListTask3.java
@@ -0,0 +1,18 @@
+package Homework;
+
+import java.util.ArrayList;
+
+public class day18_ArrayListTask3 {
+    public static void main(String[] args) {
+        //3. Write a Java program to insert an element into the array list at the first position.
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+        System.out.println(numbers);
+        numbers.add(0,60);
+        System.out.println(numbers);
+    }
+}
