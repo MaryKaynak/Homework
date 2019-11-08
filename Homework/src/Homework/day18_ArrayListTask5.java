9c281a08d7084bb345036c50ab63df24e0fff59f
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-08 17:34:46 -0500

    homework

---
+++ Homework/src/Homework/day18_ArrayListTask5.java
@@ -0,0 +1,19 @@
+package Homework;
+
+import java.util.ArrayList;
+
+public class day18_ArrayListTask5 {
+    public static void main(String[] args) {
+        //5. Write a Java program to update specific array element by given element.
+        ArrayList<String> color = new ArrayList<>();
+        color.add("Yellow");
+        color.add("Blue");
+        color.add("Gray");
+        color.add("Burgundy");
+        color.add("Orange");
+        System.out.println(color);
+        color.set(2,"White");
+        System.out.println(color);
+
+    }
+}
