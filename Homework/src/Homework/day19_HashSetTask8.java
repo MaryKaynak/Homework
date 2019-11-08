b65d7ad8e4f40ac829ce0ebb616e35bd2b18033a
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-08 09:44:00 -0500

    homework

---
+++ Homework/src/Homework/day19_HashSetTask8.java
@@ -0,0 +1,25 @@
+package Homework;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class day19_HashSetTask8 {
+    public static void main(String[] args) {
+        //8. Write a Java program to convert a hash set to a tree set.
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("Orange");
+        hashSet.add("Red");
+        hashSet.add("Pink");
+        hashSet.add("Red");
+        hashSet.add("White");
+        System.out.println(hashSet);
+        TreeSet<String> treeSet = new TreeSet<>(hashSet);
+        for (String color : treeSet) {
+            System.out.println(color);
+        }
+
+        }
+
+        }
+
+
