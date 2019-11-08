b2066f522df140a862fbf3735d810a00e1ce8385
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-08 09:57:06 -0500

    homework

---
+++ Homework/src/Homework/day19_HashSetTask9.java
@@ -0,0 +1,20 @@
+package Homework;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class day19_HashSetTask9 {
+    public static void main(String[] args) {
+        //9. Write a Java program to convert a hash set to a List/ArrayList.
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("Nissan");
+        hashSet.add("Mercedes");
+        hashSet.add("Toyota");
+        hashSet.add("Tesla");
+        hashSet.add("Audi");
+        System.out.println( hashSet);
+        ArrayList<String> arrayList = new ArrayList<>(hashSet);
+            System.out.print(arrayList);
+        }
+    }
+
