9e4afba5bed8ae7651b8f6186be3a2fdffd5514c
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-08 06:27:16 -0500

    homework

--- Homework/src/Homework/day14_ArrayTask2.java
+++ Homework/src/Homework/day14_ArrayTask2.java
@@ -2,6 +2,14 @@ package Homework;
 
 public class day14_ArrayTask2 {
     public static void main(String[] args) {
-
+      int[]  array = {10,15,20,25,30,35,40};
+      int minnumber = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            int mynumber = array[i];
+            if(mynumber < minnumber){
+                minnumber = mynumber;
+            }
+        }
+        System.out.println("Min number is: " + minnumber);
     }
 }
