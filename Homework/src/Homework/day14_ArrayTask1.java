f1ef6ddd8d5e2eebef712a324ce250f86e0bc3e9
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-08 06:21:15 -0500

    homework

---
+++ Homework/src/Homework/day14_ArrayTask1.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class day14_ArrayTask1 {
+    public static void main(String[] args) {
+        int[] myArray = {34, 5, 6, 2, 3, 78, 1};
+         int maxnumber = Integer.MIN_VALUE;
+        for (int i = 0; i < myArray.length; i++) {
+            int mynumber = myArray[i];
+            if(mynumber > maxnumber){
+                maxnumber = mynumber;
+            }
+        }
+        System.out.println(maxnumber);
+    }
+}
+
