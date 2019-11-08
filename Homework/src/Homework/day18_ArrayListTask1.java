a2d47280450dd3ec83458f862859162d09a40267
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-08 15:27:06 -0500

    homework

--- Homework/src/Homework/day18_ArrayListTask1.java
+++ Homework/src/Homework/day18_ArrayListTask1.java
@@ -1,8 +1,17 @@
 package Homework;
 
+import java.util.ArrayList;
+
 public class day18_ArrayListTask1 {
     public static void main(String[] args) {
        // 1. Write a Java program to create a new array list, add some colors (string) and print out the collection.
-
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("White");
+        colors.add("Black");
+        colors.add("Pink");
+        colors.add("Red");
+        colors.add("Orange");
+        colors.add("Purple");
+        System.out.println(colors);
     }
 }
