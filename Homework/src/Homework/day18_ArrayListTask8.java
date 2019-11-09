8fc129e22f7600e185b8a01dead57d9de1c857df
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-08 19:33:05 -0500

    homework

---
+++ Homework/src/Homework/day18_ArrayListTask8.java
@@ -0,0 +1,20 @@
+package Homework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class day18_ArrayListTask8 {
+    public static void main(String[] args) {
+        //8. Write a Java program to sort a given array list.
+        ArrayList<Character> letters = new ArrayList<>();
+        letters.add('T');
+        letters.add('H');
+        letters.add('A');
+        letters.add('M');
+        letters.add('O');
+        letters.add('K');
+        System.out.println(letters);
+        Collections.sort(letters);
+        System.out.println(letters);
+    }
+}
