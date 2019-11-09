64cdad50556cf1ac477d9aee34696f7b48ee2867
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-08 20:20:38 -0500

    homework

---
+++ Homework/src/Homework/day18_ArrayListTask10.java
@@ -0,0 +1,23 @@
+package Homework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class day18_ArrayListTask10 {
+    public static void main(String[] args) {
+        //10. Write a Java program to shuffle elements in a array list.[advanced]
+        List<String> list = new ArrayList<>();
+        list.add("Tulip");
+        list.add("Rose");
+        list.add("Lily");
+        list.add("Lavender");
+        list.add("Snowdrop");
+        System.out.println("Before suffer:" + list);
+        Collections.shuffle(list);
+        System.out.println("After suffle:");
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
