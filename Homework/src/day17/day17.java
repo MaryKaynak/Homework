99b24f852dbb7f3f322c99440f8ff1b016c9cc63
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-18 06:53:51 -0500

    homework

---
+++ Homework/src/day17/day17.java
@@ -0,0 +1,19 @@
+package day17;
+
+public class day17 {
+    public static void main(String[] args) {
+        //part4, as homework
+        //USING TWO LOOP print names and surnames
+        //ex:
+        //Bayram Guney
+        //Micheal Jackson
+            //part1
+            String[][] arr = {
+                    //   0         1         2           3            4
+                    {"Hilal", "Zeynep", "Yunus", "Micheal", "Baraka"}, //0. row
+                    {"Isbilen", "Kaynak", "Yilmaz", "Jackson", "Obama"}};   //1. row
+
+        }
+    }
+
+
