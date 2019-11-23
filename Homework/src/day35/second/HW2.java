341e56a2351a9f8b69e1c5182c0c88707abea00e
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-22 19:54:03 -0500

    homework

---
+++ Homework/src/day35/second/HW2.java
@@ -0,0 +1,42 @@
+package day35.second;
+
+public class HW2 {
+    public static void main(String[] args) {
+      Person prsn = new Person();
+        System.out.println(prsn.result());
+        Person person = new Person("Yusuf",49,70,2,"Green");
+        System.out.println("-------------------------------");
+        System.out.println(person.result());
+        Person person1 = new Person("Zeynep",16,58,2,"Brown");
+        System.out.println("--------------------------------");
+        System.out.println(person1.result());
+        Person person2 = new Person("Nihal",36,62,2,"Brown");
+        System.out.println("----------------------------------");
+        System.out.println(person2.result());
+        Person person3 = new Person();
+        System.out.println("----------------------------------");
+        System.out.println(person3.eyeColor);
+        Person person4 = new Person();
+        System.out.println("----------------------------------");
+        System.out.println(person4.age);
+        Person person5 = new Person();
+        System.out.println("----------------------------------");
+        System.out.println(person5.name);
+        Person person6 = new Person("Mahmut",67,75,2,"Browm");
+        System.out.println("----------------------------------");
+        System.out.println(person6.result());
+        Person person7 = new Person("Gul",56,75,2,"Green");
+        System.out.println("----------------------------------");
+        System.out.println(person7.result());
+        Person person8 = new Person("Mert",30,80,2,"Browm");
+        System.out.println("----------------------------------");
+        System.out.println(person8.result());
+        Person person9 = new Person("Elif",25,56,2,"Browm");
+        System.out.println("----------------------------------");
+        System.out.println(person6.result());
+
+
+
+
+    }
+}
---
+++ Homework/src/day35/second/Person.java
@@ -0,0 +1,49 @@
+package day35.second;
+
+
+public class Person {
+
+    String name;
+    int age;
+    int weight;
+    int leg;
+    String eyeColor;
+
+    public Person(){
+        age = 21;
+        weight = 80;
+        leg = 2;
+        eyeColor = "Brown";
+        name = "Yunus";
+    }
+    public Person(String name,int age){
+        this();
+        }
+        public Person(String name,int age,int leg){
+        this.name = name;
+        this.age = age;
+        this.leg = leg;
+
+        }
+        public Person(String name,int age,int weight,int leg){
+          this();
+        }
+        public Person(String nam,int ages,int weigh,int legs,String eyeColors){
+             name = nam;
+             age = ages;
+             weight = weigh;
+             leg = legs;
+             eyeColor = eyeColors;
+
+        }
+        public String result(){
+        return "Person Name: " + name + "\n"
+                + "Person age: " + age + "\n"
+                + "Person weight: " + weight + "\n"
+                + "Person leg: " + leg + "\n"
+                + "Person eye color: " + eyeColor;
+        }
+
+    }
+
+
